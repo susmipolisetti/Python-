cd1681546fca630682791606b599cca68073d665
susmipolisetti <susmipolisetti@gmail.com>
2026-06-20 00:47:14 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+while True:
+    print("\n===== Calculator =====")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "5":
+        print("Thank You!")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "1":
+        print("Result:", num1 + num2)
+
+    elif choice == "2":
+        print("Result:", num1 - num2)
+
+    elif choice == "3":
+        print("Result:", num1 * num2)
+
+    elif choice == "4":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Cannot divide by zero")
+
+    else:
+        print("Invalid choice")
